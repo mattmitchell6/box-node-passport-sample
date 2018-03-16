e3fdc2e22b467f46bb3ea03f245a2663c3036aa4
Matt Mitchell <mmitchell@box.com>
2018-03-16 11:44:27 -0700

    minor updates

--- controllers/profile.js
+++ controllers/profile.js
@@ -13,15 +13,13 @@ const BoxSdk = require('../service/box/boxSdk');
  * Fetch app user token + info
  */
 router.get('/', async function (req, res) {
-  let appUserClient = BoxSdk.getAppAuthClient('user', req.user.boxId);
+  let boxUserId = req.user.boxId
+  let appUserClient = BoxSdk.getAppAuthClient('user', boxUserId);
 
   // get app user access token and user info
   let tokens = await BoxSdk.getAppUserTokens(req.user.boxId);
   req.user.boxAccessToken = tokens.accessToken
-  let appUserInfo = await appUserClient.users.get(
-    appUserClient.CURRENT_USER_ID,
-    { fields: "name,login,created_at"}
-  );
+  let appUserInfo = await appUserClient.users.get(boxUserId, {fields: "name,login,created_at"});
 
   res.render('pages/profile', { user: req.user, appUser: appUserInfo});
 });
